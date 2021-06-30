08fca895241f65e2219b062a17f6bddcba6697b8
sumitdeshmukh9922 <sumitdeshmukh9922@gmail.com>
2021-07-01 00:53:47 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ const App = () => {
           <Route exact path='/' component={Home} />
           <Route exact path='/signin' component={Signin} />
           <Route exact path='/signup' component={Signup} />
-          <Route exact path='*' component={PageNotFound} />
+          <Route exact path='*' component={Signin} />
         </Switch>
         <Footer />
       </UserContext.Provider>
