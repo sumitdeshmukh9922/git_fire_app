a45d6b9534c1d25af0c8279eb21a2ba7d0308474
sumitdeshmukh9922 <sumitdeshmukh9922@gmail.com>
2021-07-01 00:24:47 +0530

    first commit

---
+++ src/Config/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: 'AIzaSyC6Dhz2JSmajcZaq1wp4jHSLf4jgt403os',
+  authDomain: 'mygitapp-8d6fe.firebaseapp.com',
+  projectId: 'mygitapp-8d6fe',
+  storageBucket: 'mygitapp-8d6fe.appspot.com',
+  messagingSenderId: '326976990809',
+  appId: '1:326976990809:web:51a9a28d649a03047c8b48',
+  measurementId: 'G-CS1J9S6PWJ',
+};
